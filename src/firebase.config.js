e941d809f837488e7dae43cdb5439a905d1380e5
Vicky Sonaiya <vickysonaiya0189@gmail.com>
2025-02-10 18:47:28 +0530

    firebase config changes

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   //your firebase credentials
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBiBRD1ECpC1L5v0R8fOeeppEv6qT_p37g",
+  authDomain: "mobile-verification-933b7.firebaseapp.com",
+  projectId: "mobile-verification-933b7",
+  storageBucket: "mobile-verification-933b7.firebasestorage.app",
+  messagingSenderId: "574412371120",
+  appId: "1:574412371120:web:bda063c7fff23f562eb775"
 };
 // firebase credential part over.
 
