a320c2963e46eb7a87c900ce7142f37792a98e73
Vicky Sonaiya <vickysonaiya0189@gmail.com>
2025-02-12 12:43:43 +0530

    minor changes

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,42 +1,5 @@
-// import React from 'react'
 import { CgSpinner } from "react-icons/cg";
-// import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import image from "./images";
-// import "./index.css";
-
-// const Dashboard = () => {
-//   const [loading, setLoading] = useState(false);
-//   const navigate = useNavigate();
-
-//   const handleNavigate = () => {
-//     setLoading(true);
-//     setTimeout(() => {
-//       setLoading(false);
-//       navigate("/");
-//     }, 1000);
-//   };
-
-//   return (
-//     <section className="flex flex-col items-center h-screen">
-//   <img
-//     src={image.image3}
-//     alt="Sign in illustration"
-//     className="w-66 h-96 image"
-//   />
-//   <button
-//     onClick={handleNavigate}
-//     className="absolute bottom-10 bg-black flex gap-1 items-center justify-center py-2 px-6 text-white rounded-md text-lg hover:bg-black transition duration-300"
-//   >
-//     {loading && <CgSpinner size={20} className="mt-1 animate-spin" />}
-//     <span>Go To Login</span>
-//   </button>
-// </section>
-//   );
-// };
-
-// export default Dashboard;
-
 import React, { useState } from "react";
 
 const Dashboard = () => {
@@ -114,13 +77,13 @@ const Dashboard = () => {
           </button>
         </div>
         <div className="mt-6 flex justify-center">
-        <button
-          onClick={handleNavigate}
-          className="absolute bottom-10 bg-white flex py-2 px-6 text-black rounded-md text-lg font-bold transition duration-300"
-        >
-          {loading && <CgSpinner size={20} className="mt-1 animate-spin" />}
-          <span className="mr-2 text-xl">Go To Login</span>
-        </button>
+          <button
+            onClick={handleNavigate}
+            className="absolute bottom-10 bg-white flex py-2 px-6 text-black rounded-md text-lg font-bold transition duration-300"
+          >
+            {loading && <CgSpinner size={20} className="mt-1 animate-spin" />}
+            <span className="mr-2 text-xl">Go To Login</span>
+          </button>
         </div>
       </div>
     </section>
